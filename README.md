7158ed496076c998ad945eb4660e80f4d6c0bf37
xxyy <devnull@nowak-at.net>
2014-02-28 17:25:13 +0100

    Add example usage, Add comments, Fix fade in

--- README.md
+++ README.md
@@ -3,9 +3,24 @@ doge.js
 
 Doge.js is a simple jQuery script that provides a simple interface to dynamically flood your page with doge sayings. (defined by you!)
 <br>
-To get started, just insert your own texts into the "doges" array in doge.js and include the script in your page. Note that you will need jQuery.
-<br>
 If you are still not convinced by doge.js, check it out LIVE at http://xxyy.github.io/
+
+Usage
+======
+To get started, just insert your own texts into the "doges" array in doge.js and include the script in your page. Note that you will need jQuery.
+You will also have to add the "wow" class to yout doge container (Should be `body` in most cases)
+Example page structure:
+´´´´html
+<!doctype html>
+<head ...>
+<body class="wow">
+    <!-- your awesome content here -->
+    
+    <!-- such load time improve -->
+    <script src="http://code.jquery.com/jquery-1.10.1.min.js" type="text/javascript" /><!-- It is recommended that you download jQuery and host it on your own servers -->
+    <script src="doge.js" type="text/javascript" /><!-- Achievement get! Wow -->
+</body>
+´´´´
 
 License
 =======
--- doge.js
+++ doge.js
@@ -20,28 +20,48 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 var doges = ["wow", "such doge.js", "many add ur own doge swag pls"];
 var colors = ["#0040FF", "#2EFEF7", "#DF0101", "#088A08", "#CC2EFA", "#FFBF00"];
 
+/**
+ * Builds a doge saying with color, text and position randomised.
+*/
 function buildDoge() {
+    //Need class for identification
     return "<div class='doge' style='color: "
+            // Set font color to random entry of colors array
+            // Math.random returns a number between 0 and 1
             + colors[Math.floor(Math.random() * colors.length)]
             + "; left: "
+            // Set random distance from left screen edge in %
             + Math.floor(Math.random() * 95) //don't want overflow
             + "%; top: "
+            // Set random distance from left screen edge in %
             + Math.floor(Math.random() * 95) //don't want overflow
             + "%;'>"
+            // Set the text to a random doge saying from the doges array
             + doges[Math.floor(Math.random() * doges.length)]
             + "</div>";
 }
 
+/**
+ * Displays a random doge saying, with fade-in and fade-out animations.
+ * @see buildDoge()
+*/
 function showDoge() {
+    //Appends a new doge saying to the doge container (will be the body element in most cases)
     $(".wow").append(buildDoge);
-    $(".doge").fadeIn(2000, function() {
+    // Apply a fadeIn function to all doge sayings on the page with a duration of 2 seconds.
+    // fade in won't occur without calling hide() before: 
+    $(".doge").hide().fadeIn(1500, function() {
+        //Once complete, fade all doge sayings out with a duration of 2 seconds.
         $(".doge").fadeOut(2000, function() {
+            //Once that has completed too, remove all doge sayings from the page
             $(".doge").remove();
+            //Recurse this function so a new saying is displayed
             showDoge(); // wow such recurse
         });
     });
 }
 
+//Start doge saying display loop on load
 $(window).on("load", function() {
     showDoge();
 });
